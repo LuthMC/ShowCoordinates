1d00a97b198198b31cb1042a2e98151ffc88543d
LuthMC <luthfi366822@gmail.com>
2025-02-05 19:13:48 +0700

    Update to 5.24.0

--- src/Luthfi/ShowCoordinates/Main.php
+++ src/Luthfi/ShowCoordinates/Main.php
@@ -4,6 +4,7 @@ namespace Luthfi\ShowCoordinates;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\player\Player;
 
 class Main extends PluginBase {
 
@@ -14,12 +15,21 @@ class Main extends PluginBase {
     private function startCoordinateDisplay(): void {
         $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
             foreach ($this->getServer()->getOnlinePlayers() as $player) {
-              if ($player->isOnline()) {
-                  $position = $player->getPosition();
-                  $coords = "Position: " . round($position->getX(), 1) . ", " . round($position->getY(), 1) . ", " . round($position->getZ(), 1);
-                  $player->sendActionBarMessage($coords);
-              }
+                if ($player->isOnline()) {
+                    $this->sendCoordinates($player);
+                }
             }
         }), 20);
+    }
+
+    /**
+     * Send player current coordinates as an action bar message.
+     *
+     * @param Player $player
+     */
+    private function sendCoordinates(Player $player): void {
+        $position = $player->getPosition();
+        $coords = "Position: " . round($position->getX(), 1) . ", " . round($position->getY(), 1) . ", " . round($position->getZ(), 1);
+        $player->sendActionBarMessage($coords);
     }
 }
